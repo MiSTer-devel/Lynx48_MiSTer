b5d1a722ac1af1f23e142e5b6a5a7ce1842eee3c
Alan Steremberg <alan.steremberg@gmail.com>
2022-02-25 09:05:25 -0800

    updated sys

--- Lynx_48.sv
+++ Lynx_48.sv
@@ -26,7 +26,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [45:0] HPS_BUS,
+	inout  [48:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,
@@ -36,8 +36,9 @@ module emu
 	output        CE_PIXEL,
 
 	//Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
-	output  [7:0] VIDEO_ARX,
-	output  [7:0] VIDEO_ARY,
+	//if VIDEO_ARX[12] or VIDEO_ARY[12] is set then [11:0] contains scaled size instead of aspect ratio.
+	output [12:0] VIDEO_ARX,
+	output [12:0] VIDEO_ARY,
 
 	output  [7:0] VGA_R,
 	output  [7:0] VGA_G,
@@ -47,14 +48,52 @@ module emu
 	output        VGA_DE,    // = ~(VBlank | HBlank)
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
+	output        VGA_SCALER, // Force VGA scaler
 
-	
+	input  [11:0] HDMI_WIDTH,
+	input  [11:0] HDMI_HEIGHT,
+	output        HDMI_FREEZE,
+
+`ifdef MISTER_FB
+	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
+	// FB_FORMAT:
+	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
+	//    [3]   : 0=16bits 565 1=16bits 1555
+	//    [4]   : 0=RGB  1=BGR (for 16/24/32 modes)
+	//
+	// FB_STRIDE either 0 (rounded to 256 bytes) or multiple of pixel size (in bytes)
+	output        FB_EN,
+	output  [4:0] FB_FORMAT,
+	output [11:0] FB_WIDTH,
+	output [11:0] FB_HEIGHT,
+	output [31:0] FB_BASE,
+	output [13:0] FB_STRIDE,
+	input         FB_VBL,
+	input         FB_LL,
+	output        FB_FORCE_BLANK,
+
+`ifdef MISTER_FB_PALETTE
+	// Palette control for 8bit modes.
+	// Ignored for other video modes.
+	output        FB_PAL_CLK,
+	output  [7:0] FB_PAL_ADDR,
+	output [23:0] FB_PAL_DOUT,
+	input  [23:0] FB_PAL_DIN,
+	output        FB_PAL_WR,
+`endif
+`endif
 
 	output        LED_USER,  // 1 - ON, 0 - OFF.
 
+	// b[1]: 0 - LED status is system status OR'd with b[0]
+	//       1 - LED status is controled solely by b[0]
+	// hint: supply 2'b00 to let the system control the LED.
 	output  [1:0] LED_POWER,
 	output  [1:0] LED_DISK,
 
+	// I/O board button press simulation (active high)
+	// b[1]: user button
+	// b[0]: osd button
 	output  [1:0] BUTTONS,
 
 	input         CLK_AUDIO, // 24.576 MHz
@@ -98,6 +137,20 @@ module emu
 	output        SDRAM_nCAS,
 	output        SDRAM_nRAS,
 	output        SDRAM_nWE,
+
+`ifdef MISTER_DUAL_SDRAM
+	//Secondary SDRAM
+	//Set all output SDRAM_* signals to Z ASAP if SDRAM2_EN is 0
+	input         SDRAM2_EN,
+	output        SDRAM2_CLK,
+	output [12:0] SDRAM2_A,
+	output  [1:0] SDRAM2_BA,
+	inout  [15:0] SDRAM2_DQ,
+	output        SDRAM2_nCS,
+	output        SDRAM2_nCAS,
+	output        SDRAM2_nRAS,
+	output        SDRAM2_nWE,
+`endif
 
 	input         UART_CTS,
 	output        UART_RTS,
@@ -152,7 +205,7 @@ localparam CONF_STR = {
 	"O12,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%;",
 	"-;",
 	"O34,Machine,Lynx 48K,Lynx 96k,96k Scorpion;",
-	"O5,BASIC Autostart,Off,On;",	
+	//"O5,BASIC Autostart,Off,On;",	
 	"-;",
 	"OD,Joysticks Swap,No,Yes;",
 	"R0,Reset and close OSD;",
@@ -173,6 +226,8 @@ wire        ioctl_wr;
 wire [24:0] ioctl_addr;
 wire  [7:0] ioctl_data;
 
+wire [21:0] gamma_bus;
+
 //Keyboard Ps2
 
 wire [10:0] ps2_key;
@@ -184,13 +239,13 @@ wire [5:0] joy_1 = status[13] ? joystick_0[5:0] : joystick_1[5:0];
 
 wire basic_autostart = status[5] ? 1'b1 : 1'b0;
 
-hps_io #(.STRLEN($size(CONF_STR)>>3), .PS2DIV(1103)) hps_io
+hps_io #(.CONF_STR(CONF_STR), .PS2DIV(1103)) hps_io
 (
 	.clk_sys(clk_sys),
 	.HPS_BUS(HPS_BUS),
 	.EXT_BUS(),
+	.gamma_bus(gamma_bus),
 
-	.conf_str(CONF_STR),
 	.forced_scandoubler(forced_scandoubler),
 
 	.buttons(buttons),
@@ -284,34 +339,19 @@ assign CLK_VIDEO = clk_sys;
 wire [1:0] scale = status[2:1];
 assign VGA_SL = scale ; //{scale == 3, scale == 2};
 //assign VGA_SL = 0;
+wire freeze_sync;
 
 video_mixer #(448, 1) mixer
 (
-        .clk_vid(CLK_VIDEO),
-
-        .ce_pix(ce_pix),
-        .ce_pix_out(CE_PIXEL),
+	.*,
 
         .hq2x(scale == 1),
-        .scanlines(0),
         .scandoubler (scale || forced_scandoubler),
 
         .R({video[8:6],video[8]}), 
         .G({video[5:3],video[5]}), 
         .B({video[2:0],video[2]}),
 
-        .mono(0),
-
-        .HSync(HSync),
-        .VSync(VSync),
-        .HBlank(HBlank),
-        .VBlank(VBlank),
-        .VGA_R(VGA_R),
-        .VGA_G(VGA_G),
-        .VGA_B(VGA_B),
-        .VGA_VS(VGA_VS),
-        .VGA_HS(VGA_HS),
-        .VGA_DE(VGA_DE)
 );
 
 
